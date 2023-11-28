ed62ec62491934427de12be5263e0a8eec2a1a83
sarvar-swe <s.azodov.software.engineer@gmail.com>
2023-11-28 10:50:46 +0500

    Basic Ecommerce project: CRUD operations with mvc

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Data;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<Category> Categories { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>().HasData(
+            new Category { Id = 1, Name = "Action", DisplayOrder = 1},
+            new Category { Id = 2, Name = "SciFi", DisplayOrder = 2},
+            new Category { Id = 3, Name = "History", DisplayOrder = 3}
+        );
+    }
+}
